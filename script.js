b07962542f97873bff559ca89030148b25398bc2
ayeletpackter <ayeletpackter@gmail.com>
2021-08-02 18:17:09 +0300

    adding a google map on the app

--- script.js
+++ script.js
@@ -41,3 +41,52 @@ const createTableBody = (data) => {
     myTable.appendChild(tBodyEl);
   }
 };
+
+
+
+
+function init_map() {
+  let selectorMapElement = document.querySelector('#gmap_canvas');
+  let googleMapTitle = "ATM 1";
+  let googleMapAddress ="disingov 31 Tel-Aviv";
+  let googleMapLat = 31.4037193;
+  let googleMapLong = 33.9606947;
+  
+  const myOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(googleMapLat, googleMapLong),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  let infowindow = new google.maps.InfoWindow({
+      content: `
+        <strong>${googleMapTitle}</strong>
+        <br>${googleMapAddress}<br>
+      `
+  });
+  
+  let map = new google.maps
+    .Map(selectorMapElement, myOptions);
+  
+    let marker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(31.665121, 34.585473)
+  });
+
+ let marker2 = new google.maps.Marker({
+    map: map,
+    position: new google.maps.LatLng(38.2847678, -122.9536827)
+  });
+  
+  
+
+  google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+  });
+
+  google.maps.event.addListener(marker2, 'click', function() {
+    infowindow.open(map, marker2);
+});
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
